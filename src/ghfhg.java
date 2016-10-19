ee747d4bef371f761d8741a202c5f23420dc2f75
Sseimoja <siimseims@gmail.com>
2016-10-19 16:33:31 +0300

    Initial commit

---
+++ src/ghfhg.java
@@ -0,0 +1,20 @@
+/**
+ * Created by opilane on 19.10.2016.
+ */
+import java.util.Scanner;
+public class ghfhg {public static void main(String[] args) {
+
+    int Celsiuz;
+    double Fahrenheit;
+    Scanner in = new Scanner(System.in);
+    System.out.print("How many degrees Celsius? ");
+    Celsiuz = in.nextInt();
+    Fahrenheit = Celsiuz * 9/5.0+32.0;
+    System.out.print(Celsiuz + " C = ");
+    System.out.println(Fahrenheit + " F");}
+
+
+   
+
+
+}
